f389963c42e47b26ce324122cfe2900c19069a88
vov4eg <a.vov4eg@gmail.com>
2020-03-03 00:14:25 +0300

    Please review

---
+++ homework/Vladimir Avseenkov/ex1_Counter/test/CounterTest.java
@@ -0,0 +1,25 @@
+package ex1_Counter.test;
+
+import ex1_Counter.bean.Counter;
+
+public class CounterTest {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        System.out.println("Value = " + counter.getValue());
+        System.out.println("Step = " + counter.getStep());
+
+        counter.increment();
+
+        System.out.println("Value = " + counter.getValue());
+
+        counter.setStep(5);
+        counter.increment();
+        counter.setStep(2);
+        counter.decrement();
+
+        System.out.println("Value = " + counter.getValue());
+        System.out.println("Step = " + counter.getStep());
+
+    }
+
+}
