11536cebcbdde6122f78f2be88aa6b3a3190412e
Anton Kupreichik <58224001+Enotik-code@users.noreply.github.com>
2020-03-03 23:01:17 +0300

    Create WhileOddChecker.java

---
+++ homework/Kupreichik Anton/HomeWork_2/ex1_OddNumberTable/com.company/service/WhileOddChecker.java
@@ -0,0 +1,11 @@
+package com.company.service;
+
+public class WhileOddChecker {
+    private int number;
+    public void tableOutput(){
+        while(number <= 50){
+            System.out.print(number + "->");
+            number++;
+        }
+    }
+}
