0dee74db63bdd68be652d7061987ab2999812db9
vov4eg <a.vov4eg@gmail.com>
2020-03-06 22:52:14 +0300

    brainf*ck

---
+++ homework/Vladimir Avseenkov.ex2_PrimeNumbers/PrimeNumbers.java
@@ -0,0 +1,46 @@
+public class PrimeNumbers {
+
+    public static int summ;
+    public static int count = 0;
+    public static String output = "";
+
+
+    public static void main(String[] args) {
+        printCountAndSumm();
+    }
+
+    public static void printCountAndSumm() {
+
+        for (int i = 2; i < 500; i++) {
+            boolean simple = true;
+            if (i == 17 || i == 71) {
+                continue;
+            }
+            if (count >= 50) {
+                break;
+            }
+
+            if (i == 2) {
+                addToSimpleNumbers(i);
+            } else {
+                for (int j = 2; j < i; j++) {
+                    if (i % j == 0) {
+                        simple = false;
+                    }
+                }
+                if (simple) {
+                    addToSimpleNumbers(i);
+                }
+
+            }
+
+        }
+        System.out.println("Summ = " + summ + "; Count = " + count + "\n" + output);
+    }
+
+    public static void addToSimpleNumbers(int i) {
+        summ += i;
+        count++;
+        output += i + " ";
+    }
+}
