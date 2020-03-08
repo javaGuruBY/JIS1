41e61c6459942dadb6270606ea30b5d8ec1de0d8
Бладыко Данила Евгеньевич <54392986+Bell-Kranal@users.noreply.github.com>
2020-03-08 11:43:38 +0300

    review pls

---
+++ homework/Bladyko Danila/src/by/jrr/thread/RunMyThread.java
@@ -0,0 +1,18 @@
+package by.jrr.thread;
+
+import by.jrr.test.ArrayServiceTest;
+
+public class RunMyThread implements Runnable {
+    public void run() {
+        ArrayServiceTest arrayServiceTest = new ArrayServiceTest();
+
+        try {
+            arrayServiceTest.testShouldCountSum();
+            arrayServiceTest.testShouldCountAvg();
+            arrayServiceTest.testShouldCreateArray();
+            arrayServiceTest.testShouldSortArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
