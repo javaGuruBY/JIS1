d1b0b3ebc9ec709e257a0d44983afda796ab2afe
vov4eg <a.vov4eg@gmail.com>
2020-03-10 00:59:29 +0300

    Avseenkov V HomeWork7 Ex3 Sort, Swap

--- homework/Avseenkov V/HomeWork7/Ex2_ArrayService/test/ArrayServiceTest.java
+++ homework/Avseenkov V/HomeWork7/Ex2_ArrayService/test/ArrayServiceTest.java
@@ -2,12 +2,16 @@ package test;
 
 import service.ArrayService;
 
+import java.util.Arrays;
+
 public class ArrayServiceTest {
     ArrayService arrayService = new ArrayService();
 
     public void runTests() throws Exception {
         shouldReturnAverageResult();
         shouldReturnSum();
+        shouldSortArray();
+        sholdSwapArray();
     }
 
     void shouldReturnAverageResult() throws Exception {
@@ -36,6 +40,30 @@ public class ArrayServiceTest {
 
     private void checkEqualsInteger(int expected, int actual, String description) throws Exception {
         if (expected == actual) {
+            System.out.println(description + "passed");
+        } else {
+            throw new Exception(description + "FAILED!");
+        }
+    }
+
+    void shouldSortArray () throws Exception {
+        int[] array = {1, 3, 5, 2, 4};
+        int[] expected = {1, 2, 3, 4, 5};
+        int[] actual = arrayService.sort(array);
+        String description = "Test for array sorting has ";
+        checkEqualsArray(expected, actual, description);
+    }
+
+    void sholdSwapArray () throws Exception {
+        int[] array = {1, 2, 3, 4, 5};
+        int[] expected = {5, 4, 3, 2, 1};
+        int[] actual = arrayService.swap(array);
+        String description = "Test for array swapping has ";
+        checkEqualsArray(expected, actual, description);
+    }
+
+    private void checkEqualsArray (int[] expected, int[] actual, String description) throws Exception {
+        if (Arrays.equals(expected,actual)) {
             System.out.println(description + "passed");
         } else {
             throw new Exception(description + "FAILED!");
