bd4f6bd706c6169a8261ebc363a863998267fba8
Uladzislau Urbanski <zerdonik777@gmail.com>
2020-03-14 18:08:14 +0300

    Edit App file, used by Spring Boot

--- homework/Vladislav Urbanski/Home work 9/UniqueWordCounter/src/main/java/com/vladis1350/App.java
+++ homework/Vladislav Urbanski/Home work 9/UniqueWordCounter/src/main/java/com/vladis1350/App.java
@@ -1,24 +1,11 @@
 package com.vladis1350;
 
-import com.vladis1350.bean.UniqueWordCounter;
-import com.vladis1350.configuration.SpringConfig;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(SpringConfig.class);
-
-        UniqueWordCounter uniqueWordCounter = applicationContext.getBean("uniqueWordCounter", UniqueWordCounter.class);
-        uniqueWordCounter.addWord("Hello world");
-        uniqueWordCounter.addWord("Hello world");
-        uniqueWordCounter.addWord("Hello World");
-        uniqueWordCounter.addWord("Hello Vlad");
-        uniqueWordCounter.addWord("Hello Vlad");
-        uniqueWordCounter.addWord("Hello Vlados");
-        uniqueWordCounter.addWord("Hello Vlad");
-
-        uniqueWordCounter.printWordsFrequency();
-        System.out.println(uniqueWordCounter.getMostFrequentWord());
+        SpringApplication.run(App.class, args);
     }
 }
