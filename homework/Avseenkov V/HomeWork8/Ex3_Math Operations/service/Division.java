bbf3633c1e2841496940f32397763b85c6797f5e
vov4eg <a.vov4eg@gmail.com>
2020-03-14 16:28:58 +0300

    Avseenkov V HomeWork8 Ex3 MathOps

--- homework/Avseenkov V/HomeWork8/Ex3_Math Operations/service/Division.java
+++ homework/Avseenkov V/HomeWork8/Ex3_Math Operations/service/Division.java
@@ -5,6 +5,11 @@ import interfaces.MathOperations;
 public class Division implements MathOperations {
     @Override
     public double compute(double a, double b) {
+        if (b == 0) {
+            System.out.println("На ноль делить плохо. И нельзя. Но больше нельзя, чем плохо. " +
+                    "Вот именно поэтому я верну 0");
+            return 0;
+        }
         return a / b;
     }
 
