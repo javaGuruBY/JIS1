80b3ec902bc78faab5a927f349a5e6935fb82fdd
Uladzislau Urbanski <zerdonik777@gmail.com>
2020-03-17 17:14:10 +0300

    User Validation Service, review please

---
+++ homework/Vladislav Urbanski/User Validation Service/src/main/java/com/vladis1350/exceptions/UserValidationExceptionInFirstName.java
@@ -0,0 +1,8 @@
+package com.vladis1350.exceptions;
+
+public class UserValidationExceptionInFirstName extends UserValidationException {
+
+    public UserValidationExceptionInFirstName(String message) {
+        super(message);
+    }
+}
---
+++ homework/Vladislav Urbanski/User Validation Service/src/main/java/com/vladis1350/messages/UserServiceMessage.java
@@ -0,0 +1,8 @@
+package com.vladis1350.messages;
+
+public class UserServiceMessage {
+
+    public static final String FIRST_NAME = "First name should be more 2 and less 15 symbols";
+    public static final String LAST_NAME = "Last name should be more 2 and less 15 symbols";
+    public static final String AGE = "Age should be more 0 and less than 120";
+}
