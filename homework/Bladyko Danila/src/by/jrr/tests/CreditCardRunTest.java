2d75bae105435256d8ba5411f9dcbeb764f2a57e
Бладыко Данила Евгеньевич <54392986+Bell-Kranal@users.noreply.github.com>
2020-03-24 22:21:12 +0300

    fixed

--- homework/Bladyko Danila/src/by/jrr/tests/CreditCardRunTest.java
+++
@@ -1,9 +0,0 @@
-package by.jrr.tests;
-
-public class CreditCardRunTest {
-    public static void main(String[] args) throws Exception {
-        CreditCardTest creditCardTest = new CreditCardTest();
-
-        creditCardTest.testShouldWriteFiveThousand();
-    }
-}
