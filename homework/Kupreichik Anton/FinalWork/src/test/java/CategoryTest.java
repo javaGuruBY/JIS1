2c9ce74743d9ba71ed449ccfbf3ae929168a16cd
Anton Kupreichik <58224001+Enotik-code@users.noreply.github.com>
2020-03-24 14:59:02 +0300

    Create CategoryTest.java

---
+++ homework/Kupreichik Anton/FinalWork/src/test/java/CategoryTest.java
@@ -0,0 +1,28 @@
+import com.company.bean.Category;
+import com.company.bean.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CategoryTest {
+
+    Product product[] = new Product[1];
+    Category category = new Category();
+
+    @BeforeEach
+    void setUp() {
+        product[0] = new Product("apple", new BigDecimal(12.4), new BigDecimal(0.3), "tasty apple");
+        product[0].addProduct(0l, product[0].getName());
+    }
+
+    @Test
+    void checkAddProduct() {
+        category.addProduct(product[0].getName(), "FRUIT");
+        int result = category.getMapSize();
+        assertEquals(result, 1);
+    }
+
+}
