8a376434ee4ca920f8434edd5f703f12b3c3d8e9
Anton Kupreichik <58224001+Enotik-code@users.noreply.github.com>
2020-03-25 15:29:12 +0300

    Update Book.java

--- homework/Kupreichik Anton/HomeWork_9/ex3_Library/com.company/service/Book.java
+++ homework/Kupreichik Anton/HomeWork_9/ex3_Library/com.company/service/Book.java
@@ -1,6 +1,6 @@
 package com.company.service;
 
-public class Book {
+public class Book implements Serialiazible{
     private String author;
     private String name;
 
@@ -23,5 +23,28 @@ public class Book {
     public Book(String name, String author) {
         this.author = author;
         this.name = name;
+    }
+    
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(author, book.author) &&
+                Objects.equals(name, book.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
