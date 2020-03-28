4b8182102a78762486945165ba0d0fbae84257ee
Anton Kupreichik <58224001+Enotik-code@users.noreply.github.com>
2020-03-28 16:44:58 +0300

    Create TV.java

---
+++ homework/Kupreichik Anton/HomeWork_5/ex3_TV/bean/TV.java
@@ -0,0 +1,43 @@
+package com.company.bean;
+
+public class TV {
+
+    private int currentChanel;
+    private int currentVolume;
+    private String manufacturer;
+    private boolean switchedOnOff = false;
+
+    public int getCurrentChanel() {
+        return currentChanel;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentChanel(int currentChanel) {
+        this.currentChanel = currentChanel;
+        System.out.println("Текущий канал: " + currentChanel);
+    }
+
+    public void setManufacturer(String manufacturer) {
+        System.out.println("Производитель: " + manufacturer);
+        this.manufacturer = manufacturer;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        this.currentVolume = currentVolume;
+    }
+
+    public void setSwitchedOnOff(boolean switchedOnOff) {
+        this.switchedOnOff = switchedOnOff;
+    }
+
+    public boolean isSwitchedOnOff() {
+        return switchedOnOff;
+    }
+}
