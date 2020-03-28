231d5129d4bfd11c0becc71307f5e41b0056d19d
a-vov4eg <lerO4428>
2020-03-28 13:39:26 +0300

    some fixes

--- homework/Avseenkov V/HomeWork7/Ex4_Polindrome/service/Palindrome.java
+++ homework/Avseenkov V/HomeWork7/Ex4_Polindrome/service/Palindrome.java
@@ -5,15 +5,13 @@ public class Palindrome {
     public static boolean isPalindrome(String text) {
         boolean palindrome = false;
         char[] arrayChar = string2CharConverter(text);
-        for (int i = 0; i < arrayChar.length / 2 - 1; i++) {
+        for (int i = 0; i < arrayChar.length / 2; i++) {
             if (arrayChar[i] != arrayChar[arrayChar.length - i - 1]) {
                 return false;
             } else {
-                palindrome = true;
+                return true;
             }
         }
-        return palindrome;
-    }
 
     static char[] string2CharConverter(String text) {
         text = text.toUpperCase();
