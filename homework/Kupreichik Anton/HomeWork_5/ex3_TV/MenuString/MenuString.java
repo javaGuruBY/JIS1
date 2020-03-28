526a4c1fdb18b2a4dc722bd22154ba8c338de2f7
Anton Kupreichik <58224001+Enotik-code@users.noreply.github.com>
2020-03-28 16:49:35 +0300

    Create MenuString.java

---
+++ homework/Kupreichik Anton/HomeWork_5/ex3_TV/MenuString/MenuString.java
@@ -0,0 +1,21 @@
+package com.company.MenuString;
+
+public class MenuString {
+    public static final String MENU = "1.Ввести марку телевизора\n2.Включить телевизор\n3.Выключить телевизор\n" +
+            "4.Выбрать канал\n5.Переключить на следующий канал\n6.Переключить на предыдущий канал\n" +
+            "7.Увеличить звук\n8.Уменьшить звук\n9.Выход\n0.Вывести всю информацию\n";
+    public static final String ENTER_NAME = "Введите марку телевизора";
+    public static final String TV_ON = "Телевизор включен";
+    public static final String YOU_CANT_ON_TV = "Нельзя включить включеннный телевизор";
+    public static final String YOU_CANT_OFF_TV = "Нельзя выключить выключеннный телевизор";
+    public static final String TV_OFF = "Телевизор выключен";
+    public static final String INCREASE_CHANNEL = "Включен следующий канал";
+    public static final String REDUCE_CHANNEL = "Включен предыдущий канал канал";
+    public static final String CURRENT_VOLUME = "Текущая громкость";
+    public static final String MANUFACTURER = "Марка телевизора:";
+    public static final String CURRENT_CHANNEL = "Текущий канал: ";
+    public static final String YOU_CANT_INCREASE_VOLUME = "Нельзя изменить громкость когда телевизор выключен";
+    public static final String YOU_CANT_INCREASE_CHANNEL = "Нельзя переключить канал когда телевизор выключен";
+    public static final String CHOOSE_CHANNEL = "Выберете канал";
+    public static final String ERROR = "Неверный выбор";
+}
