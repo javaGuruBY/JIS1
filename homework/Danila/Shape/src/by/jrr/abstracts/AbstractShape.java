f5c1668e8cb35a926d295819a4e5be2193b25af7
Бладыко Данила Евгеньевич <54392986+Bell-Kranal@users.noreply.github.com>
2020-03-28 18:56:41 +0300

    delete interface Shape

--- homework/Danila/Shape/src/by/jrr/abstracts/AbstractShape.java
+++ homework/Danila/Shape/src/by/jrr/abstracts/AbstractShape.java
@@ -2,18 +2,8 @@ package by.jrr.abstracts;
 
 import by.jrr.interfaces.Shape;
 
-public class AbstractShape implements Shape {
+public class AbstractShape {
     double radius;
     int height;
     int width;
-
-    @Override
-    public String getName() {
-        return null;
-    }
-
-    @Override
-    public double getArea() {
-        return 0;
-    }
 }
