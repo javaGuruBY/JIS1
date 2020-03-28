494e3da8c82b7c523dacf7b4036289362766e7c1
Anton Kupreichik <58224001+Enotik-code@users.noreply.github.com>
2020-03-29 02:14:41 +0300

    Update LibraryTest.java

--- homework/Kupreichik Anton/HomeWork_9/ex3_Library/com.company/test/LibraryTest.java
+++ homework/Kupreichik Anton/HomeWork_9/ex3_Library/com.company/test/LibraryTest.java
@@ -1,16 +1,20 @@
 package com.company.test;
 
-import com.company.service.Book;
-import com.company.service.Library;
+
+import com.company.bean.Book;
+import com.company.service.LibaryService;
 
 public class LibraryTest {
 
-    Library library = new Library();
+
+    LibaryService library = new LibaryService();
+
+    Book book1 = new  Book("автор1", "название1");
+    Book book2 = new Book("автор2", "название2");
 
     public void testShouldReturnNewCountOfBooks() throws Exception {
-        String string = "Hello world!";
-        library.addBook(new Book("автор1", "название1"));
-        int  expectedResult = library.returnSize();
+        library.addBookToLibrary(book2);
+        int expectedResult = library.getSize();
         int actualResult = 1;
 
         assertEqualsInteger(expectedResult, actualResult, "testShouldReturnNewCountOfBooks");
@@ -18,11 +22,10 @@ public class LibraryTest {
 
 
     public void testShouldReturnNewCountOfBooksAfterRemove() throws Exception {
-        String string = "Hello world!";
-        library.addBook(new Book("автор1", "название1"));
-        library.addBook(new Book("автор2", "название2"));
-        library.removeBook("автор1", "название1");
-        int  expectedResult = library.returnSize();
+        library.addBookToLibrary(book2);
+        library.addBookToLibrary(book1);
+        library.deleteBookFromLibrary(book1);
+        int expectedResult = library.getSize();
         int actualResult = 1;
 
         assertEqualsInteger(expectedResult, actualResult, "testShouldReturnNewCountOfBooksAfterRemove");
@@ -30,7 +33,7 @@ public class LibraryTest {
 
     private void assertEqualsInteger(int expected, int actual, String description) throws Exception {
         if (expected != actual) {
-            throw new Exception(description + " has failed! \n"+
+            throw new Exception(description + " has failed! \n" +
                     "Expected '" + expected + "' but was '" + actual + "'");
         } else {
             System.out.println(description + " has passed!");
