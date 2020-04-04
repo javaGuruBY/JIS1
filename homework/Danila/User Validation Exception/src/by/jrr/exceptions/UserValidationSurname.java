1108fdf61346e8bf2da41cf4d38eadcc5ed7f8d0
Бладыко Данила Евгеньевич <54392986+Bell-Kranal@users.noreply.github.com>
2020-04-04 11:42:36 +0300

    empty constructor fixed

--- homework/Danila/User Validation Exception/src/by/jrr/exceptions/UserValidationSurname.java
+++ homework/Danila/User Validation Exception/src/by/jrr/exceptions/UserValidationSurname.java
@@ -1,9 +1,6 @@
 package by.jrr.exceptions;
 
 public class UserValidationSurname extends UserValidationException {
-    public UserValidationSurname() {
-    }
-
     public UserValidationSurname(String message) {
         super(message);
     }
