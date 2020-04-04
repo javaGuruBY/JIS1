c2fd4601a6d0883d89e41ee0879d973a9f154702
Бладыко Данила Евгеньевич <54392986+Bell-Kranal@users.noreply.github.com>
2020-04-04 11:41:19 +0300

    empty constructor fixed

--- homework/Danila/User Validation Exception/src/by/jrr/exceptions/UserValidationAge.java
+++ homework/Danila/User Validation Exception/src/by/jrr/exceptions/UserValidationAge.java
@@ -1,9 +1,6 @@
 package by.jrr.exceptions;
 
 public class UserValidationAge extends UserValidationException {
-    public UserValidationAge() {
-    }
-
     public UserValidationAge(String message) {
         super(message);
     }
