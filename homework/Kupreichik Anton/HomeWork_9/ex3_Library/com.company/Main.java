e15dbd519b529a9a634e6676eeabbdacdd85c25c
Anton Kupreichik <58224001+Enotik-code@users.noreply.github.com>
2020-04-05 18:20:49 +0300

    Update Main.java

--- homework/Kupreichik Anton/HomeWork_9/ex3_Library/com.company/Main.java
+++ homework/Kupreichik Anton/HomeWork_9/ex3_Library/com.company/Main.java
@@ -1,23 +1,23 @@
 package com.company;
 import com.company.bean.Book;
-import com.company.bean.Library;
-import com.company.service.LibaryService;
+import com.company.service.LibraryService;
 
 public class Main {
 
     public static void main(String[] args) {
         Book bookOne = new Book("War and Peace", "Leo Tolstoy");
         Book bookTwo = new Book("Anna Karenina", "Leo Tolstoy");
-
-        LibaryService library = new LibaryService();
+        Book bookThree = new Book("Schoolgirl", "Tara Vestover");
+        LibraryService library = new LibraryService();
 
         library.addBookToLibrary(bookOne);
         library.addBookToLibrary(bookTwo);
+        library.addBookToLibrary(bookThree);
 
-        library.findBookByName("Anna Karenina");
-        library.returnBookWithAuthor(bookOne);
-        library.deleteBookFromLibrary(bookOne);
+        library.findBookByName("War and Peace");  //Should return bookOne
+        library.findBookByAuthor("Leo Tolstoy"); //Should return bookOne, bookTwo​
+        library.deleteBookFromLibrary(bookTwo);
 
         library.returnAllBook();
-        }
+    }
 }
