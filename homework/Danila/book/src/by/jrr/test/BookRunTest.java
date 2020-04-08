c57efd15a5be67aabd6b56bb37b4bd0c1f6ad107
Danila <danik993600@mail.ru>
2020-04-08 18:28:26 +0300

    links fixed

---
+++ homework/Danila/book/src/by/jrr/test/BookRunTest.java
@@ -0,0 +1,10 @@
+package by.jrr.test;
+
+public class BookRunTest {
+
+    public static void main(String[] args) throws Exception {
+        BookTest bookTest = new BookTest();
+
+        bookTest.testShouldWriteTrue();
+    }
+}
