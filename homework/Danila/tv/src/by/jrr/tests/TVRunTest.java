7098f7bb5f2cb2c810843e64becf68c1202304f8
Danila <danik993600@mail.ru>
2020-04-08 18:39:33 +0300

    links fixed

---
+++ homework/Danila/tv/src/by/jrr/tests/TVRunTest.java
@@ -0,0 +1,9 @@
+package by.jrr.tests;
+
+public class TVRunTest {
+    public static void main(String[] args) throws Exception {
+        TVTest tvTest = new TVTest();
+
+        tvTest.testShouldWriteSecondAndOne();
+    }
+}
